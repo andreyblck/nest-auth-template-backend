91167bc2282ca31cafd33e5450e407daf9879056
Andrey Chorniy <creatuseandr@icloud.com>
2025-05-25 18:58:22 +0300

    feat: implement authorization and authentication decorators and guards
    - Add Authorization decorator for role-based access control.
    - Introduce Authorized decorator to access user data from request.
    - Create Roles decorator to set metadata for user roles.
    - Implement AuthGuard to validate user sessions and attach user data.
    - Develop RolesGuard to enforce role-based access restrictions.
    - Update UserController to utilize new decorators for user profile access.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
+import { UserRole } from '@prisma/__generated__'
 
-@Controller('user')
+import { Authorization } from '@/auth/decorators/auth.decorator'
+import { Authorized } from '@/auth/decorators/authorizated.decorator'
+import { UserService } from '@/user/user.service'
+
+@Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
+
+	@Get('profile')
+	@HttpCode(HttpStatus.OK)
+	@Authorization()
+	public async findProfile(@Authorized('id') userId: string) {
+		return this.userService.findById(userId)
+	}
+
+	@Get('profile/:id')
+	@HttpCode(HttpStatus.OK)
+	@Authorization(UserRole.ADMIN)
+	public async findProfileById(@Param('id') id: string) {
+		return this.userService.findById(id)
+	}
 }
